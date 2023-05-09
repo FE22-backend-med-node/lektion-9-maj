df700dc342f328a7c9299b3e26e2a5c765e89946
Maja Gedda <maja.gedda@zocom.se>
2023-05-09 14:40:53 +0200

    Update and delete added

--- my-pets/index.js
+++ my-pets/index.js
@@ -32,8 +32,56 @@ let newPet = {
     animal: "Rat"
 }
 
-addNewPet(newPet)
+//addNewPet(newPet)
 
 // TODO:
-//1. Skapa en funktion som uppdaterar ett djur i myPets.json
-//2. Skapa en funktion som tar bort ett djur i myPets.json
+// 1. Skapa en funktion som uppdaterar ett djur i myPets.json
+// UPDATE
+// Pet to update
+// let updatedPet = {
+// id: 2
+// name: Babe
+// }
+
+function updatePet(id, prop, value) {
+    // Get the list of pets
+    let currPets = JSON.parse(getPets());
+    // Sort out the animal to update and change it
+    let index = currPets.findIndex((pet) => pet.id === id);
+    currPets[index][prop] = value;
+    // console.log(currPets);
+    // return the new list of pets with the updated pet
+    currPets = JSON.stringify(currPets);
+    // write to file
+    fs.writeFile('pets.json', currPets, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(JSON.parse(getPets()));
+        }
+    })
+}
+
+// updatePet(2, "owner", "Per Olsson");
+
+
+// 2. Skapa en funktion som tar bort ett djur i myPets.json
+function deletePet(id) {
+    // hämta listan över djuren
+    let currPets = JSON.parse(getPets());
+    // hitta vilket index vårt djur ligger på
+    let index = currPets.findIndex((pet) => pet.id === id);
+    // använda oss av splice för att ta bort det ur listan
+    if (index > -1) {
+        currPets.splice(index, 1);
+    }
+    // uppdatera vår pets.json mha fs.writeFile
+    fs.writeFile('pets.json', JSON.stringify(currPets), (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(JSON.parse(getPets()));
+        }
+    })
+}
+deletePet(314);
